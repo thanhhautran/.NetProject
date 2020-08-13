59dc3ca2f0fc3267f20437e178f335010b130440
Tran Hau <jackgreenbee@gmail.com>
2020-08-13 19:55:10 +0700

    The First

---
+++ Model/DAO/UserDAO.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models.DAO;
+using SshNet.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+
+namespace ProjectCore.Models.DAO
+{
+    public class UserDAO
+    {
+        public static usertable checkUser(string username, string password)
+        {
+            var p = MD5.Create().ComputeHash(Encoding.ASCII.GetBytes(password));
+            string encodedPassWord = Encoding.ASCII.GetString(p);
+            using (var context = new ProjectContext())
+            {
+                var user = context.usertable
+                  .Include(p => p.roletable).Where(p => p.username == username).
+                  Where(p => p.password == encodedPassWord).FirstOrDefault();
+                return user;
+            }
+        }
+       public static List<usertable> getAllUser()
+        {
+            using(var context = new ProjectContext())
+            {
+                var users = context.usertable.Include(p => p.roletable);
+                List < usertable > userslist = users.ToList();
+                return userslist;    
+            }    
+        }
+    }
+}
---
+++ WebShop/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Models;
+using Model.DAO;
+namespace ProjectCore.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+            return View(model);
+        }
+        public IActionResult Login(LoginModel model)
+        {
+            return View();
+        }
+    }
+}
