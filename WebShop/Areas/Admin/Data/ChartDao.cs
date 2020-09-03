d9e6f8922ecdfbbd773e5946933091a0fb98fbb5
ThanhTung212Z <tung016485@gmail.com>
2020-09-03 16:28:34 +0700

    aaaa

---
+++ WebShop/Areas/Admin/Data/ChartDao.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.Internal;
+using Project.Models.DAO;
+using ProjectCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop.Areas.Admin.Data
+{
+    public class ChartDao
+    {
+        ProjectContext context;
+
+        public ChartDao()
+        {
+            this.context = new ProjectContext();
+        }
+
+        public int[] earningByQuarter()
+        {
+            int[] result = new int[4];
+            int tmp = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                int tmp2 = tmp + 3;
+                result[i] = (int)context.DonHang.Where(d => d.ngaygiaodich.Value.Month >= tmp && d.ngaygiaodich.Value.Month <= tmp2 && d.ngaygiaodich.Value.Year == DateTime.Now.Year).Sum(s => s.giatridon);
+                tmp += 3;
+            }
+            return result;
+        }
+
+        public string[] getProductStatisticsLabel()
+        {
+            var type = context.Danhmuc.ToList();
+            List<string> list = new List<string>();
+
+            foreach (var i in type)
+            {
+                if (!list.Contains(i.loaiSanPham))
+                    list.Add(i.loaiSanPham);
+
+            }
+            return list.ToArray();
+        }
+
+        public int[] getProductStatisticsType(string[] label)
+        {
+            int[] result = new int[label.Length];
+            var products = context.Sanpham.ToList();
+
+            for (int p = 0; p < label.Length; p++)
+                foreach (var i in products)
+                { 
+                    if (i.danhmuc.loaiSanPham.Equals(label[p]))
+                    {
+                        result[p]++;
+                    }
+                }
+
+            return result;
+        }
+
+        public int[] getProductSoldByMonth(string label)
+        {
+            int[] result = new int[12];
+
+            for (int i = 0; i < 12; i++)
+            {
+                var receiptDetail = context.Chitietdonhang.Where(s => s.sanpham.danhmuc.loaiSanPham == label 
+                                                                    && s.donhang.ngaygiaodich.Value.Month == i).ToList();
+                   
+                    foreach (var j in receiptDetail)
+                    {
+                    result[i] += j.tonggia;                  
+                    }
+                   
+            }
+            return result;
+        }
+
+        public int[] getProductSoldForHome(string label)
+        {
+            int[] result = new int[12];
+            if (label.Equals("Phone"))
+            {
+                
+
+                for (int i = 0; i < 12; i++)
+                {
+                    var receiptDetail = context.Chitietdonhang.Where(s => s.sanpham.danhmuc.loaiSanPham == label
+                                                                        && s.donhang.ngaygiaodich.Value.Month == i).ToList();
+
+                    foreach (var j in receiptDetail)
+                    {
+                        result[i] += j.tonggia;
+                    }
+
+                }
+            }
+            if (!label.Equals("Phone"))
+            {
+
+
+                for (int i = 0; i < 12; i++)
+                {
+                    var receiptDetail = context.Chitietdonhang.Where(s => s.sanpham.danhmuc.loaiSanPham != label
+                                                                        && s.donhang.ngaygiaodich.Value.Month == i).ToList();
+
+                    foreach (var j in receiptDetail)
+                    {
+                        result[i] += j.tonggia;
+                    }
+
+                }
+            }
+            return result;
+        }
+
+        public int[] getStatusOfOrder(string[] labels)
+        {
+            int[] result = new int[labels.Length];
+            var order = context.DonHang.ToList();
+            for(int i = 0; i < labels.Length; i++)
+            {
+                foreach(var o in order)
+                {
+                    if (o.trangthai.Equals(labels[i]))
+                    {
+                        result[i]++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ WebShop/Areas/Admin/Models/ProductModel.cs
@@ -0,0 +1,85 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
+
+    namespace WebShop.Areas.Admin.Models
+    {
+    public class ProductModel
+    {
+        //San Pham
+
+        
+        public int khuyenmaiid { get; set; }
+        [Required(ErrorMessage = "Please enter Product Name ")]
+        public string tensanpham { get; set; }
+        [Required(ErrorMessage = "Please enter Color ")]
+        public string mausac { get; set; }
+        [Required(ErrorMessage = "Please enter Price ")]
+        public int gia { get; set; }
+       
+        public string uudai { get; set; }
+      
+        public IFormFile[] hinhanhsanpham { get; set; }
+        [Required(ErrorMessage = "Please enter the amount of product ")]
+        public int soluong { get; set; }
+       [Required(ErrorMessage = "Please choose one ")]
+        public string cosan { get; set; }
+        
+        public string noidungkhuyenmai { get; set; } 
+      
+        public int phanTramGiamGia { get; set; }
+       
+
+        //Thong so
+        [Required(ErrorMessage = "Please enter the specification id ")]
+        public int thongsoid { get; set; }
+        [Required(ErrorMessage = "Please enter product flatform ")]
+        public string hedieuhanh { get; set; }
+        [Required(ErrorMessage = "Please enter type of screen ")]
+        public string loaimanhinh { get; set; }
+        [Required(ErrorMessage = "Please enter this field ")]
+        public string dophangiai { get; set; }
+        [Required(ErrorMessage = "Please enter width ")]
+        public string manhinhrong { get; set; }
+        [Required(ErrorMessage = "Please enter back camera ")]
+        public string camerasau { get; set; }
+        [Required(ErrorMessage = "Please enter front camera ")]
+        public string cameratruoc { get; set; }
+        [Required(ErrorMessage = "Please enter this field ")]
+        public string quayphim { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string tocdocpu { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string chipset { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string ram { get; set; }
+        [Required(ErrorMessage = "Please enter this field   ")]
+        public string chipdohoa { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string rom { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string theNhoNgoai { get; set; }
+        [Required(ErrorMessage = "Please enter this field   ")]
+        public string trongluong { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string dungluongpin { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string bluetooth { get; set; }
+        [Required(ErrorMessage = "Please enter this field   ")]
+        public string wifi { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string gprs { get; set; }
+        [Required(ErrorMessage = "Please enter  this field  ")]
+        public string loaisim { get; set; }
+        [Required(ErrorMessage = "Please enter this field   ")]
+
+        //Danhmuc
+        
+        public int danhmucid { get; set; }
+    }
+}
+
